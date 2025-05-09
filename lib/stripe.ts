7d4b8097cb310ad6519f82e416dde427078ec437
Nicolas Le Borgne <nicolas.leborgne@icloud.com>
2025-05-09 23:14:06 +0200

    chore: remove Stripe integration and FAQ component

--- lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import Stripe from "stripe";
-
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("Missing Stripe secret key");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-12-18.acacia",
-});
